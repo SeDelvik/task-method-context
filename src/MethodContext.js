cbba7f7e7f94f960bc3c5a3c95059aac2d2c1f51
SeDelvik <klybnika4749@gmail.com>
2022-03-23 21:02:09 +0300

    1

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -6,10 +6,18 @@ import { EventEmitter } from './EventEmitter';
 2)В методе unsubscribe отпишитесь от события click с помощью EventEmitter.off(eventName, callback).
 В качестве callback нужно передавать тот же самый обработчик, который был передан при подписке.
  */
+function increment() {
+    this.count++;
+}
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    a: () => increment.apply(obj),
+    subscribe() {
+        EventEmitter.on('click', this.a);
+    },
+    unsubscribe() {
+        EventEmitter.off('click', this.a);
+    },
 };
 
 /*
@@ -19,7 +27,9 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
+    first(...args) {
+        this.second(args[2], args[1], args[0]);
+    },
     second() {
         // здесь ничего писать не нужно
     },
